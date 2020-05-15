982a7f4c37f319efac7d721f8155ac230b7585b9
Mettez votre pseudo <Mettez votre adresse mail>
2020-05-15 16:10:55 +0200

    testclee

---
+++ testclee.js
@@ -0,0 +1,10 @@
+const Gpio = require('onoff').Gpio;
+const button = new Gpio(12, 'in', 'both');
+
+
+button.watch((err, value) => {
+    console.log("Clée");
+    if (err) {
+        console.log("Erreur clée");
+    }
+});
